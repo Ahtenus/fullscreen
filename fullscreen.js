02b6b4a642a8a36b8392861bf531c077033ed797
Viktor Barsk <viktor@ubarskit.se>
2011-02-09 22:01:47 +0100

    Proper loading of jQuery

--- fullscreen.js
+++ fullscreen.js
@@ -1,26 +1,33 @@
+var matchString;
 var locations = ["svtplay.se","www.youtube.com"];
 var playerElement = ["#playerSwf object","embed"];
-var index;
-for(var i=0;i < locations.length;i++) {
-	if(window.location.host == locations[i]) {
-		index = i;
+var i;
+for (i = 0; i < locations.length; i++) {
+	if(window.location.host === locations[i]) {
+		matchString = playerElement[i];
 		break;
 	}
 }
-
-var iJs=function(U) { 
-	var s=document.createElement("script"); 
-	s.setAttribute("src",U); 
-	document.body.appendChild(s);
-};
-if(index === undefined) {
+function afterLoad() {
+	console.log(matchString);
+	var player = jQuery(matchString).clone();
+	jQuery("head").html("");
+	jQuery("body").html(player).css("padding","0").css("margin","0");
+	jQuery(window).resize( function() {
+		player.attr("height",jQuery(window).height()).attr("width",jQuery(window).width());
+	});
+	jQuery(window).resize();
+}
+if (matchString === undefined) {
 	alert("Sorry no support for "+window.location.host);
 } else {
-	iJs("http://code.jquery.com/jquery-1.5.min.js");
-	$(window).resize( function() {
-		player.attr("height",$(window).height()).attr("width",$(window).width());
-	});
-	var player = $(playerElement[i]).clone();
-	$("body").html(player);
-	$(window).resize();
+	if(typeof jQuery === 'undefined') { // Load jQuery unless it's allready present 
+		var s=document.createElement("script"); 
+		s.setAttribute("src","http://code.jquery.com/jquery-1.5.min.js"); 
+		document.body.appendChild(s);
+		s.onload = afterLoad();
+	}
+	else {
+		afterLoad();
+	}
 }
