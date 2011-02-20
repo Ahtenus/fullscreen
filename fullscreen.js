7d6df3eaec4ee963f42d1ad4c2542e93a6dce593
Viktor Barsk <viktor@ubarskit.se>
2011-02-20 22:43:11 +0100

    Pure javascript version

--- fullscreen.js
+++ fullscreen.js
@@ -1,34 +1,17 @@
-if(typeof jQuery === 'undefined') { // Load jQuery unless it's allready present 
-	var s=document.createElement("script"); 
-	s.setAttribute("src","http://code.jquery.com/jquery-1.5.min.js"); 
-	document.body.appendChild(s);
+function Resize() {
+	player.setAttribute("width",window.innerWidth);
+	player.setAttribute("height",window.innerHeight);
 }
-function continueWhenjQuery() {
-	if (typeof jQuery === 'undefined') {
-		setTimeout(continueWhenjQuery,200);
-		return;
-	} else {
-		var matchString = "embed";
-		var locations = ["svtplay.se"];
-		var playerElement = ["#playerSwf object"];
-		var i;
-		for (i = 0; i < locations.length; i++) { // Replace with site specific element if exists
-			if(window.location.host === locations[i]) {
-				matchString = playerElement[i];
-				break;
-			}
-		}
-		if(jQuery(matchString).length >= 1) { // Continue only if there actually is a video element
-			var player = jQuery(matchString).first().clone();
-			jQuery("head").html("");
-			jQuery("body").html(player).css("padding","0").css("margin","0");
-			jQuery(window).resize( function() {
-				player.attr("height",jQuery(window).height()).attr("width",jQuery(window).width());
-			});
-			jQuery(window).resize();
-		} else {
-			alert("Couldn't find any video elements");
-		}
-	}
+if(window.location.host === "svtplay.se") {
+	var elem = document.getElementsByTagName("object");
+	var player = elem[1].cloneNode(true);
 }
-continueWhenjQuery();
+else {
+	var elem = document.getElementsByTagName("embed");
+	var player = elem[0].cloneNode(true);
+}
+document.head.innerHTML = "<style> * { margin: 0 !important; padding: 0 !important; border:0 !important; overflow:hidden;}</style>";
+document.body.innerHTML = "";
+document.body.appendChild(player);
+window.onresize = Resize;
+Resize();
